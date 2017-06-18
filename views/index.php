37923973b1087c1076759fd81cf45069638fcf84
MHD SUBHI AL WATTAR <alwattar@live.com>
2017-06-18 14:54:54 +0300

    Adding Footer Section

--- views/index.php
+++ views/index.php
@@ -177,8 +177,8 @@ $articles_num = 8; // maximum articles on each section in main page
 		
             </div>
 	</section>
-	<section class="socialpost">
-	    <div class="container text-center">
+	<section class="hidden-xs">
+	    <div class="container ftfooter text-center">
 		<div class="fb-page" data-href="https://www.facebook.com/zaid.institution" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
                     <blockquote cite="https://www.facebook.com/zaid.institution" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/zaid.institution"><?php echo $ddlang->orga ?>‎</a></blockquote>
 		</div>
@@ -186,8 +186,78 @@ $articles_num = 8; // maximum articles on each section in main page
                     <a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/ZaidInstitution">Tweets by ZaidInstitution</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 		</div>
 	    </div>
-            
 	</section>
+	<footer class="hidden-xs">
+        <div class="container ">
+            <div class="row ">
+            <div class="col-md-4 pull-right col-xs-12">
+                <div class="footer-social">
+                    <h3>follow us </h3>
+                    <ul class="list-inline">
+                        <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
+                        <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
+                        <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
+                        <li><i class="fa fa-paper-plane" aria-hidden="true"></i></li>
+                        <li><i class="fa fa-youtube" aria-hidden="true"></i></li>
+                    </ul>
+                </div>
+                <div class="contact-us">
+                    <h3>contact_us </h3>
+                    <p><span><i class="fa fa-map-marker" aria-hidden="true"></i></span>address </p>
+                    <p><span><i class="fa fa-phone" aria-hidden="true"></i></span> phones </p>
+                    <p><span><i class="fa fa-envelope" aria-hidden="true"></i></span>our_email <span class="zaidemail">INFO@ZEYD.ORG</span></p>
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 text-center pull-right zlfooter">
+                <img src="<?php echo PUBLIC_PATH ?>/img/logo/logozaid.png"/>
+                <div>
+                    <p>
+			under_logo 
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 pull-right">
+                <h3>to_donate </h3>
+                <div class="donatebankdiv">
+                    <h5>orga_bank_acc <span> Vakıf Katılım</span></h5>
+                    <h5>acc_owner <span> ZEYD BİN SABİT YARDIMLAŞMA</span></h5>
+                    <div class="donatebank">
+                        <img src="<?php echo PUBLIC_PATH ?>/img/logo/v.png" />
+                        <p>
+                            TR83 0021 0000 0000 2797 7000 01 TL<br />
+                            TR02 0021 0000 0000 2797 7001 01 $<br />
+                            TR72 0021 0000 0000 2797 7001 02 EU
+                        </p>
+                    </div>
+                    <div class="clearfix"></div>
+                    <h6>swift_code<span> VAKFRISXXX</span></h6>
+                </div>
+                <br />
+                <div class="donatebankdiv">
+                    <h5>orga_bank_acc <span> ALBARAKA</span></h5>
+                    <h5>acc_owner<span> ZEYD BİN SABİT YARDIMLAŞMA DERNĞİ</span></h5>
+                    <div class="donatebank">
+                        <img src="<?php echo PUBLIC_PATH ?>/img/logo/b.png" />
+                        <p>
+                            TR67 0020 3000 0366 3064 0000 01 TL<br />
+                            TR40 0020 3000 0366 3064 0000 02 $<br />
+                            TR13 0020 3000 0366 3064 0000 03 EU
+                        </p>
+                    </div>
+                    <div class="clearfix"></div>
+                    <h6>swift_code<span> BTFHTRISXXX</span></h6>
+                </div>
+            </div>
+        </div>
+        <div class="text-center copyrightfooter">
+            ZEYD BİN SABİT DERNĞİ <span style="color:#828282;">© 2012 -  <?php echo date("Y"); ?></span>
+        </div>
+        </div>
+        
+        
+    </footer>
+    
+    
 	<script src="<?php echo JS_PATH ?>/jquery-1.12.4.min.js"></script>
 	<script src="<?php echo JS_PATH ?>/bootstrap.min.js"></script>
 	<script src="<?php echo JS_PATH ?>/npm.js"></script>
