cedd0c1b2c2685ec4adc0a456d022c35cf0eb65c
MHD SUBHI AL WATTAR <alwattar@live.com>
2017-06-19 16:26:08 +0300

    LOGIN STYLE

--- views/admin/login.php
+++ views/admin/login.php
@@ -1,15 +1,26 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8"/>
-        <title>Document</title>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link href="https://fonts.googleapis.com/css?family=Amatic+SC|Anton|Cairo|Dancing+Script|Fjalla+One|Gloria+Hallelujah|Lateef|Lato|Lobster|Open+Sans|Pacifico|Play|Ravi+Prakash|Reem+Kufi|Roboto|Shadows+Into+Light" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo CSS_PATH ?>/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo CSS_PATH ?>/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo CSS_PATH ?>/animate.css">
+	<link rel="stylesheet" href="<?php echo CSS_PATH ?>/iview.css">
+	<link rel="stylesheet" href="<?php echo CSS_PATH ?>/style.css">
     </head>
     <body>
-	<form action="" method="post">
-	    <input name="username" type="text" placeholder="username"/><br/>
-	    <input name="password" type="password" placeholder="password"/><br/>
-	    <input name="_token" type="hidden" value="<?php echo $this->_token ?>"/>
-	    <button>Login</button>
-	</form>
+	<section class="login-form">
+	    <div class="container">
+	        <form action="" method="post" autocomplete="off">
+                <h2>تسجيل الدخول</h2>
+	            <input name="username" type="text" placeholder="اسم المستخدم" autocomplete="off" /><br/>
+	            <input name="password" type="password" placeholder="كلمة المرور"/><br/>
+	            <input name="_token" type="hidden" value="<?php echo $this->_token ?>"/>
+	            <button>تسجيل الدخول</button>
+	        </form>
+	    </div>
+	</section>
     </body>
 </html>
