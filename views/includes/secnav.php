60332af22a5ddcbd0cf96d06fa79c7691acc5a81
MHD SUBHI AL WATTAR <alwattar@live.com>
2017-06-21 14:02:43 +0300

    style nav

--- views/includes/secnav.php
+++ views/includes/secnav.php
@@ -2,11 +2,11 @@
 	<section class="secnav text-center">
             <nav>
 		<ul>
-                    <li>القسم التعليمي</li>
-                    <li>القسم الخدمي</li>
-                    <li>قسم الأسرة </li>
-                    <li>القسم الدعوي</li>
-                    <li>قسم المشاريع</li>
+                    <a href=""><li>القسم التعليمي</li></a>
+                    <a href=""><li>القسم الخدمي</li></a>
+                    <a href=""><li>قسم الأسرة </li></a>
+                    <a href=""><li>القسم الدعوي</li></a>
+                    <a href=""><li>قسم المشاريع</li></a>
 		</ul>
             </nav>
 	</section>
