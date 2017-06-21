54c507b1d60cacb5bbad4e7645044ac96eb9c742
root <root@localhost.localdomain>
2017-06-21 17:51:01 +0000

    Fixing main links

--- views/includes/secnav.php
+++ views/includes/secnav.php
@@ -1,13 +1,13 @@
 
-	<section class="secnav text-center">
-            <nav>
-		<ul>
-                    <a href=""><li>القسم التعليمي</li></a>
-                    <a href=""><li>القسم الخدمي</li></a>
-                    <a href=""><li>قسم الأسرة </li></a>
-                    <a href=""><li>القسم الدعوي</li></a>
-                    <a href=""><li>قسم المشاريع</li></a>
-		</ul>
-            </nav>
-	</section>
-	s
+<section class="secnav text-center">
+    <nav>
+	<ul>
+            <a href=""><li>القسم التعليمي</li></a>
+            <a href=""><li>القسم الخدمي</li></a>
+            <a href=""><li>قسم الأسرة </li></a>
+            <a href=""><li>القسم الدعوي</li></a>
+            <a href=""><li>قسم المشاريع</li></a>
+	</ul>
+    </nav>
+</section>
+s
