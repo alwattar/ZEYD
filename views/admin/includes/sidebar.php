3e21691efd342419119b42c05ca83c9b0e762b5c
MHD SUBHI AL WATTAR <alwattar@live.com>
2017-06-21 17:47:08 +0300

    SIDEBAR

--- views/admin/includes/sidebar.php
+++ views/admin/includes/sidebar.php
@@ -5,36 +5,40 @@
                <h2>ZEYD.ORG</h2>
                <img src="<?php echo PUBLIC_PATH ?>/img/logo/logozaid.png">
                 <ul>
-                    <li><i class="fa fa-home" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/index">Dashboard</a>
-                    </li>
-                    <li><i class="fa fa-newspaper-o" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/new-article">New Article</a>
-                    </li>
+                    <a href="<?php echo ADMIN_PATH ?>/index"><li><i class="fa fa-home" aria-hidden="true"></i>
+                            Dashboard
+                        </li></a>
+                    <a href="<?php echo ADMIN_PATH ?>/new-article"><li><i class="fa fa-newspaper-o" aria-hidden="true"></i>
+                            New Article
+                        </li></a>
 
-                    <li><i class="fa fa-sitemap" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/new-section">New Section</a>
-                    </li>
+                    <a href="<?php echo ADMIN_PATH ?>/new-section"><li><i class="fa fa-sitemap" aria-hidden="true"></i>
+                            New Section
+                        </li></a>
 
-                    <li><i class="fa fa-pencil-square" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/manage-art">Manage Articles</a>
-                    </li>
+                    <a href="<?php echo ADMIN_PATH ?>/manage-art"><li><i class="fa fa-pencil-square" aria-hidden="true"></i>Manage Articles
+                        </li></a>
 
-		    <li><i class="fa fa-bar-chart" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/manage-statics">Manage ZEYD Statics</a>
-                    </li>
+		    <a href="<?php echo ADMIN_PATH ?>/manage-statics"><li><i class="fa fa-bar-chart" aria-hidden="true"></i>Manage Statics</li>
+              </a>
+                   
+                    <a href="<?php echo ADMIN_PATH ?>/manage-sliders"><li><i><img src="<?php echo PUBLIC_PATH ?>/img/39123-200.png"></i>
+                            Manage Slider
+                        </li></a>
 
-                    <li><i class="fa fa-user-plus" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/new-user">Create New User</a>
-                    </li>
+                    <a href="<?php echo ADMIN_PATH ?>/new-user"><li><i class="fa fa-user-plus" aria-hidden="true"></i>
+                            Create New User
+                        </li></a>
 
-                    <li><i class="fa fa-users" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/manage-users">Manage Users</a>
-                    </li>
+                    <a href="<?php echo ADMIN_PATH ?>/manage-users"><li><i class="fa fa-users" aria-hidden="true"></i>
+                            Manage Users
+                        </li></a>
 
-                    <li><i class="fa fa-power-off" aria-hidden="true"></i>
-                        <a href="<?php echo ADMIN_PATH ?>/logout">Logout</a>
-                    </li>
+                    <a href="<?php echo ADMIN_PATH ?> /logout">
+                           <li><i class="fa fa-power-off" aria-hidden="true"></i>
+                            Logout
+                        </li>
+                    </a>
                 </ul>
             </div>
         </div>
