2497dc1b81a2097571d05ea2bb6a059cdb117246
MHD SUBHI AL WATTAR <alwattar@live.com>
2017-06-22 23:35:18 +0300

    ستايل لوحة التحكم

--- views/admin/includes/sidebar.php
+++ views/admin/includes/sidebar.php
@@ -2,44 +2,46 @@
             <div></div>
             <div></div>
             <div id="side-div">
-               <h2>ZEYD.ORG</h2>
-               <img src="<?php echo PUBLIC_PATH ?>/img/logo/logozaid.png">
-                <ul>
-                    <a href="<?php echo ADMIN_PATH ?>/index"><li><i class="fa fa-home" aria-hidden="true"></i>
-                            Dashboard
-                        </li></a>
-                    <a href="<?php echo ADMIN_PATH ?>/new-article"><li><i class="fa fa-newspaper-o" aria-hidden="true"></i>
-                            New Article
-                        </li></a>
-
-                    <a href="<?php echo ADMIN_PATH ?>/new-section"><li><i class="fa fa-sitemap" aria-hidden="true"></i>
-                            New Section
-                        </li></a>
-
-                    <a href="<?php echo ADMIN_PATH ?>/manage-art"><li><i class="fa fa-pencil-square" aria-hidden="true"></i>Manage Articles
-                        </li></a>
-
-		    <a href="<?php echo ADMIN_PATH ?>/manage-statics"><li><i class="fa fa-bar-chart" aria-hidden="true"></i>Manage Statics</li>
-              </a>
-                   
-                    <a href="<?php echo ADMIN_PATH ?>/manage-sliders"><li><i><img src="<?php echo PUBLIC_PATH ?>/img/39123-200.png"></i>
-                            Manage Slider
-                        </li></a>
-
-                    <a href="<?php echo ADMIN_PATH ?>/new-user"><li><i class="fa fa-user-plus" aria-hidden="true"></i>
-                            Create New User
-                        </li></a>
-
-                    <a href="<?php echo ADMIN_PATH ?>/manage-users"><li><i class="fa fa-users" aria-hidden="true"></i>
-                            Manage Users
-                        </li></a>
-
-                    <a href="<?php echo ADMIN_PATH ?> /logout">
-                           <li><i class="fa fa-power-off" aria-hidden="true"></i>
-                            Logout
-                        </li>
-                    </a>
-                </ul>
+                <div >
+                   <h2>ZEYD.ORG</h2>
+                   <img src="<?php echo PUBLIC_PATH ?>/img/logo/logozaid.png">
+                    <ul>
+                        <a href="<?php echo ADMIN_PATH ?>/index"><li><i class="fa fa-home" aria-hidden="true"></i>
+                                Dashboard
+                            </li></a>
+                        <a href="<?php echo ADMIN_PATH ?>/new-article"><li><i class="fa fa-newspaper-o" aria-hidden="true"></i>
+                                New Article
+                            </li></a>
+                
+                        <a href="<?php echo ADMIN_PATH ?>/new-section"><li><i class="fa fa-sitemap" aria-hidden="true"></i>
+                                New Section
+                            </li></a>
+                
+                        <a href="<?php echo ADMIN_PATH ?>/manage-art"><li><i class="fa fa-pencil-square" aria-hidden="true"></i>Manage Articles
+                            </li></a>
+                
+                            <a href="<?php echo ADMIN_PATH ?>/manage-statics"><li><i class="fa fa-bar-chart" aria-hidden="true"></i>Manage Statics</li>
+                  </a>
+                       
+                        <a href="<?php echo ADMIN_PATH ?>/manage-sliders"><li><i><img src="<?php echo PUBLIC_PATH ?>/img/39123-200.png"></i>
+                                Manage Slider
+                            </li></a>
+                
+                        <a href="<?php echo ADMIN_PATH ?>/new-user"><li><i class="fa fa-user-plus" aria-hidden="true"></i>
+                                Create New User
+                            </li></a>
+                
+                        <a href="<?php echo ADMIN_PATH ?>/manage-users"><li><i class="fa fa-users" aria-hidden="true"></i>
+                                Manage Users
+                            </li></a>
+                
+                        <a href="<?php echo ADMIN_PATH ?> /logout">
+                               <li><i class="fa fa-power-off" aria-hidden="true"></i>
+                                Logout
+                            </li>
+                        </a>
+                    </ul>
+                </div>
             </div>
         </div>
         
