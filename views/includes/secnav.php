a15a0748e9eff6cc255c8f7a3f5502a73660f314
root <root@localhost.localdomain>
2017-06-30 08:08:49 +0000

    No public section appears

--- views/includes/secnav.php
+++ views/includes/secnav.php
@@ -3,7 +3,9 @@
 <section class="secnav text-center">
     <nav>
 	<ul>
-	    <?php foreach($this->sections as $sect){ ?>
+	    <?php foreach($this->sections as $sect){
+if($sect->sec_id == 1) continue;
+?>
 		<a data-value="section_<?php echo stripslashes(strip_tags($sect->sec_id)) ?>" href="#"><li><?php
 													   $sect = (array) $sect;
 													   echo strip_tags(stripslashes($sect['sec_name_' . $_SESSION['dlang']]));
